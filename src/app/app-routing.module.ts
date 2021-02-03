d0aabec635764b7e0167a27e85106073774180ed
Ubenthiran <60597269+Ubenthiran@users.noreply.github.com>
2021-02-03 17:46:46 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
+
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+
+
+const routes: Routes = [
+{
+	path:'login',
+	component:LoginComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes),FormsModule,ReactiveFormsModule],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
